2db4a195d5ea68e0a68411197f7d395dd5e2c8a7
Nishit Gupta <nishit.gupta6708@gmail.com>
2021-04-14 16:59:51 +0530

    gst error dialog visibility

--- app/src/main/java/com/billbook/app/activities/BillingNewActivity.java
+++ app/src/main/java/com/billbook/app/activities/BillingNewActivity.java
@@ -166,8 +166,8 @@ public class BillingNewActivity extends AppCompatActivity implements NewBillingA
         else
             serialNumber = MyApplication.getInVoiceNumberForNonGst();
         bill_no.setText("Bill Number:"+serialNumber);
+        GSTError.setVisibility(MyApplication.showGstPopup() > 0 ? serialNumber <= 2 ? View.VISIBLE : View.GONE : View.GONE);
         if(isGSTAvailable){
-            GSTError.setVisibility(View.GONE);
             gstType.setVisibility(View.VISIBLE);
             gstPercentage.setVisibility(View.VISIBLE);
             tvAmountBeforeTax.setVisibility(View.VISIBLE);
@@ -182,10 +182,6 @@ public class BillingNewActivity extends AppCompatActivity implements NewBillingA
             gstTitle.setVisibility(View.VISIBLE);
 
         }else{
-            if(serialNumber<=2)
-            GSTError.setVisibility(View.VISIBLE);
-            else
-                GSTError.setVisibility(View.GONE);
             gstType.setVisibility(View.GONE);
             gstPercentage.setVisibility(View.GONE);
             tvAmountBeforeTax.setVisibility(View.GONE);
