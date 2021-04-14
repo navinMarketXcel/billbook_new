fff7b86dc66f635917489c43c96f96224b1a3efe
Nishit Gupta <nishit.gupta6708@gmail.com>
2021-04-14 16:56:52 +0530

    reuse bill number

--- app/src/main/java/com/billbook/app/activities/BillingNewActivity.java
+++ app/src/main/java/com/billbook/app/activities/BillingNewActivity.java
@@ -691,12 +691,12 @@ public class BillingNewActivity extends AppCompatActivity implements NewBillingA
                                 no = MyApplication.getInVoiceNumber();
                             else
                                 no = MyApplication.getInVoiceNumberForNonGst();
-                            if(Integer.parseInt(invSerialNo.getText().toString())<no){
-                                DialogUtils.showToast(c,"Bill number already exists");
-                            }else{
-                                serialNumber = Integer.parseInt(invSerialNo.getText().toString());
-                                bill_no.setText("Bill Number:"+invSerialNo.getText().toString());
-                            }
+//                            if(Integer.parseInt(invSerialNo.getText().toString())<no){
+//                                DialogUtils.showToast(c,"Bill number already exists");
+//                            }else{
+//                            }
+                            serialNumber = Integer.parseInt(invSerialNo.getText().toString());
+                            bill_no.setText("Bill Number:"+invSerialNo.getText().toString());
                         }
                         dismiss();
                     break;
