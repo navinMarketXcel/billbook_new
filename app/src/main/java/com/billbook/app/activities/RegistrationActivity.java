479feb04310e9137740bacb516bde5a527b18601
Nishit Gupta <nishit.gupta6708@gmail.com>
2021-07-07 18:14:10 +0530

    refactor registration activity w/ viewBinding

--- app/src/main/java/com/billbook/app/activities/RegistrationActivity.java
+++ app/src/main/java/com/billbook/app/activities/RegistrationActivity.java
@@ -4,19 +4,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.TextView;
-
+import com.billbook.app.databinding.ActivityRegistrationBinding;
 import com.billbook.app.utils.Util;
 import com.google.gson.Gson;
 import com.billbook.app.R;
@@ -30,6 +22,7 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -42,20 +35,23 @@ import retrofit2.Response;
 
 public class RegistrationActivity extends AppCompatActivity {
     private final String TAG = "RegistrationActivity";
-    private EditText userNameEdt, shopNameEdt, firstName, lastName, shopAddr, mobileNo, pinCodeEdt;
-
-    private ArrayList<String> stateList = new ArrayList<>();
-    private ArrayList<String> citiesList = new ArrayList<>();
+    private final ArrayList<String> stateList = new ArrayList<>();
+    private final ArrayList<String> citiesList = new ArrayList<>();
     private ArrayAdapter<String> cityAdpater;
     private ArrayAdapter<String> stateAdpater;
     private JSONArray citiesJSONArray = null;
-    private String mobileNoText, OTP;
-    private AutoCompleteTextView states, city;
+    private String mobileNoText,OTP;
+
+    private ActivityRegistrationBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+
+        binding = ActivityRegistrationBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
         setTitle("Registration");
         initUI();
         initAdapter();
@@ -81,14 +77,8 @@ public class RegistrationActivity extends AppCompatActivity {
     private void initUI() {
         mobileNoText = getIntent().getExtras().getString("mobileNo");
         OTP = getIntent().getExtras().getString("otp");
-        userNameEdt = findViewById(R.id.userNameEdt);
-        shopNameEdt = findViewById(R.id.shopNameEdt);
-        shopAddr = findViewById(R.id.shopAddressEdt);
-        mobileNo = findViewById(R.id.mobileNoEdt);
-        mobileNo.setText(mobileNoText);
-        states = findViewById(R.id.state);
-        pinCodeEdt = findViewById(R.id.pinCodeEdt);
-        city = findViewById(R.id.city);
+
+        binding.mobileNoEdt.setText(mobileNoText);
     }
 
     private void initAdapter() {
@@ -96,48 +86,49 @@ public class RegistrationActivity extends AppCompatActivity {
         cityAdpater = new ArrayAdapter<String>(
                 RegistrationActivity.this,
                 R.layout.spinner_item, citiesList);
-        city.setAdapter(cityAdpater);
+        binding.city.setAdapter(cityAdpater);
 
 
         stateAdpater = new ArrayAdapter<String>(
                 RegistrationActivity.this,
                 R.layout.spinner_item, stateList);
-        states.setAdapter(stateAdpater);
+        binding.state.setAdapter(stateAdpater);
 
     }
 
     private boolean validateData() {
-        if (shopNameEdt.getText().toString().isEmpty()) {
+       if (binding.shopNameEdt.getText().toString().isEmpty()) {
             DialogUtils.showToast(this, "Shop name can not be empty.");
             return false;
-        } else if (shopAddr.getText().toString().isEmpty()) {
+        }  else if (binding.shopAddressEdt.getText().toString().isEmpty()) {
             DialogUtils.showToast(this, "Shop address can not be empty.");
             return false;
-        } else if (mobileNo.getText().toString().isEmpty() || mobileNo.getText().toString().length() < 10) {
+        } else if (binding.mobileNoEdt.getText().toString().isEmpty() || binding.mobileNoEdt.getText().toString().length() < 10) {
             DialogUtils.showToast(this, "Please enter valid mobile number");
             return false;
-        } else if (states.getText().toString().isEmpty()) {
-            DialogUtils.showToast(this, "Please select State");
-            return false;
-        } else if (city.getText().toString().isEmpty()) {
-            DialogUtils.showToast(this, "Please select city");
-            return false;
-        } else if (pinCodeEdt.getText().toString().length() < 6) {
-            DialogUtils.showToast(this, "Please enter valid pin code");
-            return false;
-        } else return true;
+        }else if (binding.state.getText().toString().isEmpty()) {
+           DialogUtils.showToast(this, "Please select State");
+           return false;
+       }else if (binding.city.getText().toString().isEmpty()) {
+           DialogUtils.showToast(this, "Please select city");
+           return false;
+       } else if(binding.pinCodeEdt.getText().toString().length()<6){
+           DialogUtils.showToast(this, "Please enter valid pin code");
+           return false;
+       }
+        else return true;
     }
 
     public void registerUser(View view) {
         if (validateData()) {
             Map<String, String> headerMap = new HashMap<>();
-            headerMap.put("shopName", shopNameEdt.getText().toString());
-            headerMap.put("shopAddr", shopAddr.getText().toString());
-            headerMap.put("mobileNo", mobileNo.getText().toString());
-            headerMap.put("state", states.getText().toString());
-            headerMap.put("city", city.getText().toString());
-            headerMap.put("pincode", pinCodeEdt.getText().toString());
-            headerMap.put("otp", OTP);
+            headerMap.put("shopName",binding.shopNameEdt.getText().toString());
+            headerMap.put("shopAddr",binding.shopAddressEdt.getText().toString());
+            headerMap.put("mobileNo",binding.mobileNoEdt.getText().toString());
+            headerMap.put("state",binding.state.getText().toString());
+            headerMap.put("city",binding.city.getText().toString());
+            headerMap.put("pincode",binding.pinCodeEdt.getText().toString());
+            headerMap.put("otp",OTP);
             register(headerMap);
         }
     }
@@ -164,7 +155,7 @@ public class RegistrationActivity extends AppCompatActivity {
                     if (citiesJSONArray == null)
                         citiesJSONArray = new JSONArray(readJSONFromAsset());
                     for (int i = 0; i < citiesJSONArray.length(); i++) {
-                        if (states.getText().toString().equalsIgnoreCase(
+                        if (binding.state.getText().toString().equalsIgnoreCase(
                                 citiesJSONArray.getJSONObject(i).getString("state"))) {
                             citiesList.add(citiesJSONArray.getJSONObject(i).getString("name"));
                         }
@@ -198,7 +189,7 @@ public class RegistrationActivity extends AppCompatActivity {
             byte[] buffer = new byte[size];
             is.read(buffer);
             is.close();
-            json = new String(buffer, "UTF-8");
+            json = new String(buffer, StandardCharsets.UTF_8);
             Log.v(TAG, json);
         } catch (IOException ex) {
             ex.printStackTrace();
