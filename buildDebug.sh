38b5e946b169613b642a377ee3519445ee90ba10
Nishit Gupta <nishit.gupta6708@gmail.com>
2021-11-26 17:27:23 +0530

    feat: Scripts to build Release and debug apk + Signing Config for release configuration

---
+++ buildDebug.sh
@@ -0,0 +1,1 @@
+./gradlew clean &&  ./gradlew assembleDebug && open app/build/outputs/apk/debug/
---
+++ buildRelease.sh
@@ -0,0 +1,1 @@
+./gradlew clean &&  ./gradlew assembleRelease && open app/build/outputs/apk/release/
