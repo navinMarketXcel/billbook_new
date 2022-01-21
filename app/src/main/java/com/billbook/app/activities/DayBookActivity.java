77468c8258c49697fb6e6c88d239d75c7c3dc4af
hariom1625 <hariomyadav.btech.cs19@iiitranchi.ac.in>
2022-01-21 11:45:37 +0530

    refactor: error message

--- app/src/main/java/com/billbook/app/activities/DayBookActivity.java
+++ app/src/main/java/com/billbook/app/activities/DayBookActivity.java
@@ -237,7 +237,7 @@ public class DayBookActivity extends AppCompatActivity {
                 @Override
                 public void onFailure(Call<Object> call, Throwable t) {
                     DialogUtils.stopProgressDialog();
-                    DialogUtils.showToast(DayBookActivity.this,"Failed to load Day Book. Please check your internet connection");
+                    DialogUtils.showToast(DayBookActivity.this,"Please check your Internet Connection");
                 }
             });
         } catch (JSONException e) {
@@ -319,7 +319,7 @@ public class DayBookActivity extends AppCompatActivity {
                 @Override
                 public void onFailure(Call<Object> call, Throwable t) {
                     DialogUtils.stopProgressDialog();
-                    DialogUtils.showToast(DayBookActivity.this,"Some error occured. Please check your internet connection");
+                    DialogUtils.showToast(DayBookActivity.this,"Please check your Internet Connection");
                 }
             });
         } catch (JSONException e) {
