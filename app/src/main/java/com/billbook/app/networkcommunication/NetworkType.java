f1008f82c86161b486ab8d7d125bc7eedbf16a67
ashish017 <ashish.r@market-xcel.com>
2022-07-29 12:48:03 +0530

    network issue

---
+++ app/src/main/java/com/billbook/app/networkcommunication/NetworkType.java
@@ -0,0 +1,49 @@
+package com.billbook.app.networkcommunication;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+
+public class NetworkType {
+    public String getNetworkClass(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        if (info == null || !info.isConnected())
+            return "-"; // not connected
+        if (info.getType() == ConnectivityManager.TYPE_WIFI)
+            return "WIFI";
+        if (info.getType() == ConnectivityManager.TYPE_MOBILE) {
+            int networkType = info.getSubtype();
+            switch (networkType) {
+                case TelephonyManager.NETWORK_TYPE_GPRS:
+                case TelephonyManager.NETWORK_TYPE_EDGE:
+                case TelephonyManager.NETWORK_TYPE_CDMA:
+                case TelephonyManager.NETWORK_TYPE_1xRTT:
+                case TelephonyManager.NETWORK_TYPE_IDEN:     // api< 8: replace by 11
+                case TelephonyManager.NETWORK_TYPE_GSM:      // api<25: replace by 16
+                    return "2G";
+                case TelephonyManager.NETWORK_TYPE_UMTS:
+                case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                case TelephonyManager.NETWORK_TYPE_EVDO_A:
+                case TelephonyManager.NETWORK_TYPE_HSDPA:
+                case TelephonyManager.NETWORK_TYPE_HSUPA:
+                case TelephonyManager.NETWORK_TYPE_HSPA:
+                case TelephonyManager.NETWORK_TYPE_EVDO_B:   // api< 9: replace by 12
+                case TelephonyManager.NETWORK_TYPE_EHRPD:    // api<11: replace by 14
+                case TelephonyManager.NETWORK_TYPE_HSPAP:    // api<13: replace by 15
+                case TelephonyManager.NETWORK_TYPE_TD_SCDMA: // api<25: replace by 17
+                    return "3G";
+                case TelephonyManager.NETWORK_TYPE_LTE:      // api<11: replace by 13
+                case TelephonyManager.NETWORK_TYPE_IWLAN:    // api<25: replace by 18
+                case 19: // LTE_CA
+                    return "4G";
+                case TelephonyManager.NETWORK_TYPE_NR:       // api<29: replace by 20
+                    return "5G";
+                default:
+                    return "?";
+            }
+        }
+        return "?";
+    }
+}
